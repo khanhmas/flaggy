c314d008a925b88a6720240a2aa903a11e00b1e6
alex <luquankhanh68@gmail.com>
2020-12-31 13:56:17 -0800

    init tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,3 +16,11 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    img {
+      display: inline;
+    }
+  }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    content: ["./public/**/*.html", "./src/**/*.vue"],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
+  theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
