a25172f29a0ea77dd3e8169ac3fcf9b0ff85f715
alex <luquankhanh68@gmail.com>
2021-01-06 11:02:50 -0800

    fix typing

--- src/store/actions/country.ts
+++ src/store/actions/country.ts
@@ -4,7 +4,7 @@ import { Country } from '@/types/country';
 export default {
     async fetchCountries({ commit }: {commit: any}): Promise<any> {
         const result: Array<any> = await CountryService.fetchAll();
-        const countries: Array<Country> = result.map((country: any) => {
+        const countries: Array<Country> = result.map((country: Country) => {
             return {
                 ...country,
                 population: Number(country.population).toLocaleString()
