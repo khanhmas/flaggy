51587104fff3aeef2cbb4c8b9bc45695a66ed700
alex <luquankhanh68@gmail.com>
2021-01-17 04:03:16 -0800

    add discovery note about transition element in flagcard component; add overflow-y-scroll into body to prevent flickering because of the scrollbar

--- src/components/FlagCard.vue
+++ src/components/FlagCard.vue
@@ -1,35 +1,30 @@
+<!--
+    DISCOVERY: The transition element (from Vue 3) will remove the equivalent classes in the rootElement inside it. For example: if we add "transition" class into enter-active-class attribute in transition element, it will also be removed from the class in the div below
+-->
 <template>
-    <!-- <transition
-        name="scale"
-        mode="out-in"
-        enter-from-class="scale-0"
-        enter-active-class="transition duration-700 ease-in-out transform"
-        enter-to-class="scale-100"
-    > -->
-        <div
-            @mouseover="onHover()"
-            @mouseleave="onLeave()"
-            class="inline-block w-full min-h-full transition transform rounded-xl flag-container hover:scale-110"
-            :id="name"
-        >
-            <img
-                loading="lazy"
-                ref="flag_image"
-                class="object-cover object-center w-full md:max-h-screen flag-image rounded-t-md lg:w-full lg:h-48"
-                :src="flag"
-                alt="country card"
-            />
-            <div class="p-6 flaggy-frost rounded-b-xl">
-                <h2
-                    class="mb-4 text-lg font-extrabold leading-none text-left truncate"
-                    :title="name"
-                >
-                    {{ name }}
-                </h2>
-                <FlagLabelInfo :labelValues="labelValues" />
-            </div>
+    <div
+        @mouseover="onHover()"
+        @mouseleave="onLeave()"
+        class="inline-block w-full min-h-full transition transform rounded-xl flag-container hover:scale-110"
+        :id="name"
+    >
+        <img
+            loading="lazy"
+            ref="flag_image"
+            class="object-cover object-center w-full md:max-h-screen flag-image rounded-t-md lg:w-full lg:h-48"
+            :src="flag"
+            alt="country card"
+        />
+        <div class="p-6 flaggy-frost rounded-b-xl">
+            <h2
+                class="mb-4 text-lg font-extrabold leading-none text-left truncate"
+                :title="name"
+            >
+                {{ name }}
+            </h2>
+            <FlagLabelInfo :labelValues="labelValues" />
         </div>
-    <!-- </transition> -->
+    </div>
 </template>
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
@@ -58,26 +53,21 @@ export default class FlagCard extends Vue implements FlagCardLabel {
     capital!: string;
 
     labelValues: Array<{ label: string; value: unknown }> = [];
-    showable: boolean = false;
+    // showable: boolean = false;
 
     created(): void {
-        setTimeout(() => {
-            this.showable = true;
-        }, 1000);
+        // setTimeout(() => {
+        //     this.showable = true;
+        // }, 1000);
         this.labelValues = initLabelValues(
             FLAG_CARD_TEXT_FIELDS,
             this as Record<string, unknown>
         );
     }
 
-    mounted(): void {
-        // setTimeout(() => {
-        //     this.showable = true;
-        // }, 1000);
-    }
-
     onHover(): void {
         (this.$refs['flag_image'] as any)?.classList.add('animate-pulse');
+        // (this.$refs['container'] as any)?.classList.add('transition', 'transform');
     }
 
     onLeave(): void {
