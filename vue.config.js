3a4115f2116a1cef6ae9c2180070a3f956cb2cb8
alex <luquankhanh68@gmail.com>
2021-01-30 11:47:58 -0800

    add base href / to fix unexpected token > while refreshing on detail page

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,7 @@ module.exports = {
             background_color: '#42b983',
         },
         workboxOptions: {
-            // exclude: [/_redirects/, /\.map$/],
-            exclude: [/_redirects/],
+            exclude: [/_redirects/, /\.map$/],
             runtimeCaching: [
                 {
                     urlPattern: new RegExp(
@@ -28,9 +27,9 @@ module.exports = {
                     urlPattern: new RegExp(
                         '^https://flaggy-unsplash.herokuapp.com/unsplash/search'
                     ),
-                    handler: 'StaleWhileRevalidate',
+                    handler: 'NetworkFirst',
                     options: {
-                        // networkTimeoutSeconds: 20,
+                        networkTimeoutSeconds: 20,
                         cacheName: 'api-photo-cache',
                         cacheableResponse: {
                             statuses: [0, 200],
@@ -50,9 +49,9 @@ module.exports = {
                 },
                 {
                     urlPattern: new RegExp('^https://images.unsplash.com/'),
-                    handler: 'StaleWhileRevalidate',
+                    handler: 'NetworkFirst',
                     options: {
-                        // networkTimeoutSeconds: 20,
+                        networkTimeoutSeconds: 20,
                         cacheName: 'asset-photo-cache',
                         cacheableResponse: {
                             statuses: [0, 200],
