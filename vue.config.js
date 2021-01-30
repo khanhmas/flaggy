2cc72057dc578243c5071851cc41d1d5d891b9f4
alex <luquankhanh68@gmail.com>
2021-01-30 10:04:22 -0800

    exclude _redirects in workboxOptions for netlify

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
             background_color: '#42b983',
         },
         workboxOptions: {
+            exclude: [/_redirects/],
             runtimeCaching: [
                 {
                     urlPattern: new RegExp(
